d9341849a9bfc68ce23da4bf4293b744e6b602f0
IdanS <idans2468@gmail.com>
2020-12-17 22:14:55 +0200

    Initial Enemy spawn using bizaire path
    Initial Enemy spawn using bizaire path

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
+using UnityEngine.Serialization;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private PathCreator pathCreated;
+    [SerializeField] private float speed = 5f;
+    private float distanceTraveled;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        distanceTraveled = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceTraveled += speed * Time.deltaTime;
+        transform.position = pathCreated.path.GetPointAtDistance(distanceTraveled);
+        transform.rotation = pathCreated.path.GetRotationAtDistance(distanceTraveled);
+        var x = transform.eulerAngles.x;
+        var y = transform.eulerAngles.y;
+        var z = transform.eulerAngles.z;
+        transform.rotation = Quaternion.Euler(x, y + 180, z);
     }
 }
