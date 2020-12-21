d499c253ecb2297329c8a2fe7c9bf49b3d7c4599
Anton Loubman <kublakhan@cs.huji.ac.il>
2020-12-21 20:48:21 +0200

    Junk commit

--- Assets/Scripts/ThrowerController.cs
+++ Assets/Scripts/ThrowerController.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using PathCreation;
-using UnityEditor;
-using UnityEngine.Serialization;
 
 public class ThrowerController : MonoBehaviour
 {
     [SerializeField] private float height = 5f;
     [SerializeField] private float throwForce = 2f;
-    [SerializeField]private Transform startLocalTransformThrowingObj;
-    [SerializeField] private GameObject _target;
+    private Vector3 _orgLocalPosThrowObj;
     private Vector3 _orgLocalPosTargetObj;
     private List<GameObject> _throwingObjects;
     private GameObject _currThrowingObj;
-    // private PathFollow _pathFollow;
+    private PathFollow _pathFollow;
 
-   
+    [SerializeField] private GameObject _target;
     // Forward/left/right vectors at time of fire
     private Vector3 _targetOriginalForward; 
     private Vector3 _targetOriginalLeft; 
@@ -28,49 +25,41 @@ public class ThrowerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _throwingObjects = new List<GameObject>();
+        foreach (Transform child in gameObject.transform)
+        {
+            _throwingObjects.Add(child.gameObject);
+        }
+
         _isAiming = false;
         _orgLocalPosTargetObj = _target.transform.localPosition;
+        SetNextThrowingObj();
     }
-    
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void SetNextThrowingObj()
+    {
+        if (_throwingObjects.Count == 0)
+        {
+            Debug.LogWarning("Finished all objects");
+            return;
+        }
+
+        if (_throwingObjects.Count == 1)
+        {
+            _throwingObjects.Add(Instantiate(_throwingObjects.First(), transform));
+        }
+
+        _currThrowingObj = _throwingObjects.First();
+        _throwingObjects.RemoveAt(0);
+        _orgLocalPosThrowObj = _currThrowingObj.transform.localPosition;
+        _pathFollow = _currThrowingObj.GetComponent<PathFollow>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // Only move target if aiming
-       
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            EnterAimMode();
-        }
-        if (_isAiming)
-        {
-            
-            HandleAimModePhysics();
-        }
-       
-
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            if (_currThrowingObj == null)
-            {
-                return;
-            }
-            _isAiming = false;
-            _target.transform.parent = gameObject.transform;
-            
-            var targetLoc = _target.transform.position;
-            _target.transform.localPosition = _orgLocalPosTargetObj;
-            
-            _currThrowingObj.transform.SetParent(null);
-            
-            _target.SetActive(false);
-            Throw(targetLoc);
-            _currThrowingObj = null;
-        }
-    }
-
-    private void HandleAimModePhysics()
-    {
+<<<<<<< HEAD
         // If -135 <= degree <= -45 (i.e. facing down) switch A and D 
         var degree = Mathf.Atan2(-_targetOriginalForward.z, -_targetOriginalForward.x) * Mathf.Rad2Deg;
         if (Input.GetKey(KeyCode.A))
@@ -93,51 +82,78 @@ public class ThrowerController : MonoBehaviour
             }
         }
         else if (Input.GetKey(KeyCode.D))
+=======
+        // Only move target if aiming
+        if (_isAiming)
+>>>>>>> parent of b729120... Removed fake throwing objects and reformated code
         {
-            if (degree >= -135f && degree <= -45f)
+            // If -135 <= degree <= -45 (i.e. facing down) switch A and D 
+            var degree = Mathf.Atan2(-_targetOriginalForward.z, -_targetOriginalForward.x) * Mathf.Rad2Deg;
+            if (Input.GetKey(KeyCode.A))
             {
-                _target.transform.position += _targetOriginalLeft * (throwForce * Time.deltaTime);
+                if (degree >= -135f && degree <= -45f)
+                {
+                    _target.transform.position += _targetOriginalRight * throwForce * Time.deltaTime;
+                }
+                else
+                {
+                    _target.transform.position += _targetOriginalLeft * throwForce * Time.deltaTime;
+                }
             }
-            else
+            else if (Input.GetKey(KeyCode.D))
             {
-                _target.transform.position += _targetOriginalRight * (throwForce * Time.deltaTime);
+                if (degree >= -135f && degree <= -45f)
+                {
+                    _target.transform.position += _targetOriginalLeft * throwForce * Time.deltaTime;
+                }
+                else
+                {
+                    _target.transform.position += _targetOriginalRight * throwForce * Time.deltaTime;
+                }
             }
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            if (_currThrowingObj == null)
+            // _currThrowingObj.SetActive(true);
+            // _currThrowingObj.transform.position += transform.forward * (throwForce * Time.deltaTime);
+
+            // Decouple _target, save directions if only started aiming
+            _target.transform.parent = null;
+            if (_isAiming == false)
             {
-                return;
+                _isAiming = true;
+                _targetOriginalForward = transform.forward;
+                _targetOriginalLeft = -transform.right;
+                _targetOriginalRight = transform.right;
             }
 
+            _target.SetActive(true);
             _target.transform.position += _targetOriginalForward * (throwForce * Time.deltaTime);
         }
-    }
 
-    // ReSharper disable Unity.PerformanceAnalysis
-    private void EnterAimMode()
-    {
-        // Decouple _target, save directions if only started aiming
-        if (_currThrowingObj == null)
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            Debug.LogWarning("No Object to throw");
-            return;
+            _isAiming = false;
+            _target.transform.parent = gameObject.transform;
+            
+            var targetLoc = _target.transform.position;
+            _target.transform.localPosition = _orgLocalPosTargetObj;
+            
+            // For temporary brown spheres
+            _currThrowingObj.SetActive(true);
+            _currThrowingObj.transform.SetParent(null);
+            
+            _target.SetActive(false);
+            Throw(targetLoc);
+            SetNextThrowingObj();
         }
-
-        _target.transform.parent = null;
-        _isAiming = true;
-        _targetOriginalForward = transform.forward;
-        _targetOriginalLeft = -transform.right;
-        _targetOriginalRight = transform.right;
-        _target.SetActive(true);
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
-    // Maybe change to DOTween
     public void Throw(Vector3 target)
     {
-        target.y = -4.5f / 2; // for now just manual caluculation
+        target.y = -4.5f / 2;
         var start = _currThrowingObj.transform.position;
         // Debug.DrawLine(start, target, Color.red, 2f);
         var midPoint = (target + start) / 2;
@@ -150,20 +166,17 @@ public class ThrowerController : MonoBehaviour
         };
         var ballisticPathGO = new GameObject();
         var pc = ballisticPathGO.AddComponent<PathCreator>();
-        var pathFollow = _currThrowingObj.GetComponent<PathFollow>();
         pc.bezierPath = path;
-        
-        pathFollow.pathCreator = pc;
-        pathFollow.pathObj = ballisticPathGO;
+
+        _pathFollow.pathCreator = pc;
+        _pathFollow.pathObj = ballisticPathGO;
     }
 
     public void AddObjToThrow(GameObject obj)
     {
         obj.transform.SetParent(transform);
-        obj.transform.localPosition = startLocalTransformThrowingObj.localPosition;
-        obj.transform.localRotation = startLocalTransformThrowingObj.localRotation;
-        _currThrowingObj = obj;
-        // _throwingObjects.Insert(0, obj);
-        // SetNextThrowingObj();
+        obj.transform.localPosition = _orgLocalPosThrowObj;
+        _throwingObjects.Insert(0, obj);
+        SetNextThrowingObj();
     }
 }
