3d777e57cb63a5fb065a8db1173dd5c5b6f2b0bf
IdanS <idans2468@gmail.com>
2020-12-23 15:54:38 +0200

    Upgraded ObjectSpawner to use object pooling
    Upgraded ObjectSpawner to use object pooling

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -1,23 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] private Bounds spawnBounds;
 
-    [SerializeField] private List<GameObject> _enemies;
-    [SerializeField] private List<GameObject> _jews;
-
+    private Queue<GameObject> _enemiesPool;
+    private Queue<GameObject> _jewsPool;
+    [SerializeField] private List<GameObject> _jewsInGame;
+    [SerializeField] private List<GameObject> _enemiesInGame;
+    private Queue<GameObject> myQueue;
+    [SerializeField] private int maxJews = 20;
+    [SerializeField] private int maxEnemies = 20;
     [SerializeField] private float minDistanceBetweenObjects;
+
+    [Header("Containers")] [SerializeField]
+    private Transform jewsPoolContainer;
+
+    [SerializeField] private Transform enemiesPoolContainer;
+    [SerializeField] private Transform jewsInGameContainer;
+    [SerializeField] private Transform enemiesInGameContainer;
+
+    enum ObjectType
+    {
+        Jew,
+        Enemy
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        _enemies = new List<GameObject>();
-        _jews = new List<GameObject>();
+        _enemiesPool = new Queue<GameObject>();
+        _jewsPool = new Queue<GameObject>();
+        _jewsInGame = new List<GameObject>();
+        _enemiesInGame = new List<GameObject>();
+        InitializeQueue(maxJews, ObjectType.Jew);
+        InitializeQueue(maxEnemies, ObjectType.Enemy);
         TestSpawn();
+    }
+
+    private void InitializeQueue(int numOfObject, ObjectType objNameToInitialize)
+    {
+        var queue = objNameToInitialize == ObjectType.Jew ? _jewsPool : _enemiesPool;
+        var container = objNameToInitialize == ObjectType.Jew ? jewsPoolContainer : enemiesPoolContainer;
+        var newObj = Resources.Load(Enum.GetName(typeof(ObjectType), objNameToInitialize)) as GameObject;
+        for (int i = 0; i < numOfObject; i++)
+        {
+            var obj = Instantiate(newObj, container, true);
+            obj.SetActive(false);
+            queue.Enqueue(obj);
+        }
     }
 
     // Update is called once per frame
@@ -43,7 +81,7 @@ public class ObjectSpawner : MonoBehaviour
             seq.AppendInterval(1);
         }
 
-        seq.Play();
+        seq.Play().OnComplete(() =>KillJew(_jewsInGame[0]));
     }
 
     public Vector3 RandomPointInBounds()
@@ -59,7 +97,7 @@ public class ObjectSpawner : MonoBehaviour
     {
         var bestDist = float.PositiveInfinity;
         GameObject closestJew = null;
-        foreach (var jew in _jews)
+        foreach (var jew in _jewsInGame)
         {
             var currDist = Vector3.Distance(pos, jew.transform.position);
             if (currDist < bestDist)
@@ -72,31 +110,52 @@ public class ObjectSpawner : MonoBehaviour
         return closestJew;
     }
 
-    private GameObject SpawnObject(string objName)
+    private GameObject SpawnObject(ObjectType objType)
     {
-        var newObj = Resources.Load(objName) as GameObject;
+        var objPoolQueue = objType == ObjectType.Jew ? _jewsPool : _enemiesPool;
+        var container = objType == ObjectType.Jew ? jewsInGameContainer : enemiesInGameContainer;
         var extent = new Vector3(minDistanceBetweenObjects, 0, minDistanceBetweenObjects);
         var pos = RandomPointInBounds();
-        while (Physics.OverlapBox(pos, extent).Length > 0)
+        while (Physics.OverlapBox(pos, extent / 2).Length > 0)
         {
             pos = RandomPointInBounds();
         }
 
+        var newObj = objPoolQueue.Dequeue();
         newObj.transform.position = pos;
-        Instantiate(newObj, pos, Quaternion.identity);
+        newObj.transform.SetParent(container);
+        newObj.SetActive(true);
         return newObj;
     }
 
     public void SpawnJew()
     {
-        var enemy = SpawnObject("Enemy");
-        _enemies.Add(enemy);
+        var jew = SpawnObject(ObjectType.Jew);
+        _jewsInGame.Add(jew);
     }
 
     public void SpawnEnemy()
     {
-        var jew = SpawnObject("Jew");
-        _jews.Add(jew);
+        var jew = SpawnObject(ObjectType.Enemy);
+        _enemiesInGame.Add(jew);
+    }
+
+    public void KillJew(GameObject jew)
+    {
+        Debug.Log("Killing Jew");
+        _jewsInGame.Remove(jew);
+        jew.SetActive(false);
+        jew.transform.SetParent(jewsPoolContainer);
+        _jewsPool.Enqueue(jew);
+    }
+
+    public void KillEnemy(GameObject enemy)
+    {
+        Debug.Log("Killing Enemy");
+        _enemiesInGame.Remove(enemy);
+        enemy.SetActive(false);
+        enemy.transform.SetParent(jewsPoolContainer);
+        _enemiesPool.Enqueue(enemy);
     }
 
     private void OnDrawGizmos()
